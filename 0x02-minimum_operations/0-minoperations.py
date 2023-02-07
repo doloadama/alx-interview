e8d60b25ebc98e2dd477696d7b469ba90fca6fb6
doloadama <doloadama@outlook.com>
2023-02-07 22:47:58 +0000

    output error fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,21 +11,14 @@ def minOperations(n: int) -> int:
     Returns an integer
     If n is impossible to achieve, return 0
     """
-    # return 0 if n is less than or equal to 0, which means it is impossible to achieve
     if n <= 0:
         return 0
-    # start checking from 2 as the first prime number
+    operations = 0
     i = 2
-    count = 0
-    # loop until i is less than or equal to n
     while i <= n:
-        # check if n is divisible by i
         while n % i == 0:
-            # increment the count by 1 for each division
-            count += 1
-            # divide n by i and update n
-            n = n // i
-        # increment i by 1
+            operations += i
+            n /= i
         i += 1
-    # return the count which is the minimum number of operations needed
-    return count
+    return operations
+
