ee7732555972d9d66b91437355739cab5a830847
doloadama <doloadama@outlook.com>
2023-02-15 17:40:19 +0000

    .

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
+
 import sys
 
-# Initialize variables to keep track of metrics
-file_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-num_lines = 0
+# Initialize variables
+total_size = 0
+status_codes = {}
 
-# Read input line by line
-for line in sys.stdin:
+# Define function to print metrics
+def print_metrics():
+    print("Total file size:", total_size)
+    for code in sorted(status_codes):
+        print(code, ":", status_codes[code])
+
+# Read from stdin line by line
+for i, line in enumerate(sys.stdin):
     try:
-        # Extract the file size and status code from the line
-        fields = line.strip().split()
-        if len(fields) == 7:
-            status_code = int(fields[5])
-            if status_code in status_codes:
-                file_size += int(fields[6])
-                status_codes[status_code] += 1
-                num_lines += 1
+        # Parse line in the format <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+        ip, _, _, _, _, status_code, file_size = line.split()
+        status_code = int(status_code)
+        file_size = int(file_size)
+        # Update metrics
+        total_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            status_codes[status_code] = 1
+        # Print metrics every 10 lines
+        if (i + 1) % 10 == 0:
+            print_metrics()
     except ValueError:
-        pass
+        # Skip lines that don't match the format
+        continue
+    except KeyboardInterrupt:
+        # Print metrics and exit on keyboard interruption
+        print_metrics()
+        sys.exit(0)
 
-    # Print metrics after every 10 lines or keyboard interruption
-    if num_lines % 10 == 0:
-        print("Total file size:", file_size)
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print("{}: {}".format(code, status_codes[code]))
-
-# Print final metrics after all input has been processed
-print("Total file size:", file_size)
-for code in sorted(status_codes.keys()):
-    if status_codes[code] > 0:
-        print("{}: {}".format(code, status_codes[code]))
+# Print final metrics
+print_metrics()
