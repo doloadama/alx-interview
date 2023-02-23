db2b73ec3520cb2a74ea0657662022b18325b831
doloadama <doloadama@outlook.com>
2023-02-23 08:07:12 +0000

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def validUTF8(data) -> bool:
+    """
+    Return True or False if data isvalid UTF-8 encoding else return False
+    """
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
