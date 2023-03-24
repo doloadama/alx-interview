204bbbf3e9369c2dc19823596fb745fa6c03f0dd
doloadama <doloadama@outlook.com>
2023-03-24 21:18:42 +0000

    pycodestyle error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,19 +16,19 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     # Initialize table
     dp = [float('inf')]*(total+1)
     dp[0] = 0
-    
+
     # Build up table
     for coin in coins:
         for i in range(coin, total+1):
             if coin <= i:
                 dp[i] = min(dp[i], dp[i-coin]+1)
-    
+
     # Check if total amount can be made
     if dp[total] == float('inf'):
         return -1
-    
+
     return dp[total]
