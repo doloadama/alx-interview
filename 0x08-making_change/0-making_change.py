5dbb3f56e55d5710d084553b503d4eeb9d50ce91
doloadama <doloadama@outlook.com>
2023-03-24 21:20:58 +0000

    code efficiency correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""
-0. Change comes from within
-"""
+# -*- coding: utf-8 -*-
 
 
 def makeChange(coins, total):
     """_summary_
 
     Args:
-        coins (_type_): a list of the values of the coins
-        total (_type_): is 0 or less return 0
+        coins (_type_): _description_
+        total (_type_): _description_
 
     Returns:
         _type_: _description_
     """
     if total <= 0:
         return 0
-
-    # Initialize table
-    dp = [float('inf')]*(total+1)
-    dp[0] = 0
-
-    # Build up table
+    
+    coins.sort(reverse=True) # Sort coins in descending order
+    num_coins = 0
+    
     for coin in coins:
-        for i in range(coin, total+1):
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i-coin]+1)
-
-    # Check if total amount can be made
-    if dp[total] == float('inf'):
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+    
+    if total > 0: # Check if total amount can be made
         return -1
-
-    return dp[total]
+    
+    return num_coins
