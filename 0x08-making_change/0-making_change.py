cb954c5d6321167ae3ae1b5f3a3ab13c6c050237
doloadama <doloadama@outlook.com>
2023-06-14 20:59:27 +0000

    pycodetyle error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,8 @@
 
 
 def makeChange(coins, total):
-    # Initialize an array to store the minimum number of coins required for each amount
+    # Initialize an array to store the minimum
+    #  number of coins required for each amount
     minCoins = [float('inf')] * (total + 1)
 
     # Base case: 0 coins needed for total amount of 0
@@ -15,8 +16,10 @@ def makeChange(coins, total):
     for coin in coins:
         # Update the minimum number of coins for each amount
         for amount in range(coin, total + 1):
-            # Choose the minimum between the current minimum and using the current coin
-            minCoins[amount] = min(minCoins[amount], minCoins[amount - coin] + 1)
+            # Choose the minimum between the current minimum
+            #  and using the current coin
+            minCoins[amount] = min(minCoins[amount],
+                                    minCoins[amount - coin] + 1)
 
     # Check if the total amount can be met by any number of coins
     if minCoins[total] == float('inf'):
