44c114e5815109644a03afa11fb85e6ef6d328e5
doloadama <doloadama@outlook.com>
2023-06-20 21:17:06 +0000

    .

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,22 +4,27 @@
 
 def island_perimeter(grid):
     """
-    function that returns the perimeter of the island
-    described in grid
+    Calculate the perimeter of an island described in a grid.
+
+    Args:
+        grid (list): A list of lists representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+
     """
-    perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
+    perimeter = 0
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                # Start with the assumption of a full perimeter
-                # Check adjacent cells and reduce the perimeter accordingly
-                if i > 0 and grid[i-1][j] == 1:
+
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
     return perimeter
