0de536c502550ddbcb834a59a9848e7e365c930b
doloadama <doloadama@outlook.com>
2023-06-20 21:07:57 +0000

    syntax correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+# Island Perimeter
+
+
 def island_perimeter(grid):
+    """
+    function that returns the perimeter of the island
+    described in grid
+    """
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -6,7 +14,8 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with the assumption of a full perimeter
+                perimeter += 4  
+                # Start with the assumption of a full perimeter
 
                 # Check adjacent cells and reduce the perimeter accordingly
                 if i > 0 and grid[i-1][j] == 1:
