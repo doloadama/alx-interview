4f6cd1b7c83331723a95cee3a54be7fa39b7ded3
doloadama <doloadama@outlook.com>
2023-06-27 23:07:17 +0000

    .

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,13 +3,18 @@
 
 def isWinner(x, nums):
     """
-    Maria and Ben are playing a game. Given a set of consecutive integers starting
-    from 1 up to and including n, they take turns choosing a prime number from the
-    set and removing that number and its multiples from the set. The player that 
+    Maria and Ben are playing a game. Given a set of
+    consecutive integers starting
+    from 1 up to and including n, they take turns 
+    choosing a prime number from the
+    set and removing that number and its multiples 
+    from the set. The player that 
     cannot make a move loses the game.
 
-    They play x rounds of the game, where n may be different for each round. 
-    Assuming Maria always goes first and both players play optimally,
+    They play x rounds of the game, where n may be 
+    different for each round. 
+    Assuming Maria always goes first and both 
+    players play optimally,
     determine who the winner of each game is.
 
     Prototype: def isWinner(x, nums)
@@ -52,7 +57,7 @@ def isWinner(x, nums):
         else:
             ben_wins += 1
 
-    
+
     return "Maria" if maria_wins > ben_wins else "Ben"
 
 if __name__ == "__main__":
