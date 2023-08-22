b004127f96be24d895c441e1826497aabe1ee531
doloadama <doloadama@outlook.com>
2023-08-22 15:13:58 +0000

    .

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize an array to store the minimum number of coins needed for each value
+    """Initialize an array to store the minimum
+       number of coins needed for each value
+    """
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
@@ -17,7 +20,3 @@ def makeChange(coins, total):
         return -1
 
     return dp[total]
-
-# Test cases
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
